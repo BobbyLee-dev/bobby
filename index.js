61bb67af51717b7d35fb631626b3a45b25b39721
Bobby Lee <rdlee8181@gmail.com>
2021-06-01 21:00:01 -0700

    Feat: Added welcom

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 #!/usr/bin/env node
 
-const clearConsole = require('clear-any-console');
-clearConsole();
+const pkgJSON = require('./package.json');
+const welcome = require('cli-welcome');
+
+welcome({
+  title: pkgJSON.name,
+  tagLine: `Get to know Bobby`,
+  description: pkgJSON.description,
+  version: pkgJSON.version,
+  bgColor: `#FADC00`,
+  color: `#000000`,
+  bold: true,
+  clear: true
+})
+
 
 console.log(`
-Bobby Lee
+Bobby Lee 
 
 Senior Front End Developer at Rosemont Media.
 
