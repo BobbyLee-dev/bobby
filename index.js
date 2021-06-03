32bc06ea43118f41aadc9ff372760b10157bec56
Bobby Lee <rdlee8181@gmail.com>
2021-06-03 14:04:59 -0700

    test: :art: Testing Conventional Commits
    Testing a longer description
    
    No breaking changes

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ welcome({
   clear: true
 })
 
+
 log(`${chalk.greenBright(`I am a Senior Front End Developer specializing in JavaScript currently focusing on React. I often utilize my favorite CMS – WordPress – to build custom themes and plugins that suit specific needs for clients and their content, in my opinion that is the key to building easy to use, maintainable, and scalable projects that perform well over time. I’m based out of San Diego California and work at Rosemont Media.`)}
 
 ${chalk.whiteBright.bgBlue.bold(` https://bobbylee.dev/ `)}
