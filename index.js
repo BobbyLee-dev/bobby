912eee8a66678f5e8b4e03fbda18847e2cc678b3
Bobby Lee <rdlee8181@gmail.com>
2021-06-02 21:17:23 -0700

    📦 NEW: Colors and Bio

--- index.js
+++ index.js
@@ -2,22 +2,22 @@
 
 const pkgJSON = require('./package.json');
 const welcome = require('cli-welcome');
+const chalk = require('chalk');
+const log = console.log;
 
 welcome({
   title: pkgJSON.name,
-  tagLine: `Get to know Bobby`,
+  tagLine: `Hi, nice to meet you!`,
   description: pkgJSON.description,
   version: pkgJSON.version,
   bgColor: `#FADC00`,
-  color: `#000000`,
+  color: `#8A2BE2`,
   bold: true,
   clear: true
 })
 
+log(`${chalk.greenBright(`I am a Senior Front End Developer specializing in JavaScript currently focusing on React. I often utilize my favorite CMS – WordPress – to build custom themes and plugins that suit specific needs for clients and their content, in my opinion that is the key to building easy to use, maintainable, and scalable projects that perform well over time. I’m based out of San Diego California and work at Rosemont Media.`)}
 
-console.log(`
-Bobby Lee 
-
-Senior Front End Developer at Rosemont Media.
+${chalk.whiteBright.bgBlue.bold(` https://bobbylee.dev/ `)}
 
 `)
